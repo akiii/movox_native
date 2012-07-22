9d299f6fb2f2fc2b06514273eb358cec1a897ea1
akiii <akiii@sfc.wide.ad.jp>
2012-07-22 18:28:28 +0900

    Added a new view controller.

---
+++ Movox/Main/MVRootSettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  MVRootSettingsViewController.h
+//  Movox
+//
+//  Created by 哲史 深谷 on 12/07/22.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MVRootSettingsViewController : UIViewController
+
+@end
