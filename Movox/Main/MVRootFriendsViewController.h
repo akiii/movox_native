3f4d3b75f5d47b1b1e699e0dbfdd0ce8cd138411
akiii <akiii@sfc.wide.ad.jp>
2012-07-27 05:39:42 +0900

    Added friends view controller.

---
+++ Movox/Main/MVRootFriendsViewController.h
@@ -0,0 +1,13 @@
+//
+//  MVRootFriendsViewController.h
+//  Movox
+//
+//  Created by 哲史 深谷 on 12/07/22.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MVRootFriendsViewController : UIViewController
+
+@end
---
+++ Movox/Main/MVRootFriendsViewController.m
@@ -0,0 +1,43 @@
+//
+//  MVRootFriendsViewController.m
+//  Movox
+//
+//  Created by 哲史 深谷 on 12/07/22.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "MVRootFriendsViewController.h"
+
+@interface MVRootFriendsViewController ()
+
+@end
+
+@implementation MVRootFriendsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
