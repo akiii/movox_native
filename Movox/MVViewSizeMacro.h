04dfe4921ceb125889cc962ff9eb22da6a85b184
akiii <akiii@sfc.wide.ad.jp>
2012-07-27 07:26:32 +0900

    Added a new header.

---
+++ Movox/MVViewSizeMacro.h
@@ -0,0 +1,12 @@
+//
+//  MVViewSizeMacro.h
+//  Movox
+//
+//  Created by 哲史 深谷 on 12/07/27.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#define SizeOfScreen    [UIScreen mainScreen].bounds.size
+#define SizeOfStatusBar [UIApplication sharedApplication].statusBarFrame.size
+#define SizeOfNavigationBar CGSizeMake(SizeOfScreen.width, 40)
+#define SizeOfToolBar   CGSizeMake(SizeOfScreen.width, 49)
