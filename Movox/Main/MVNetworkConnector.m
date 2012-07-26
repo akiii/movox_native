c42c7b2d7d157fe2eb524bf846e79931a1bac961
akiii <akiii@sfc.wide.ad.jp>
2012-07-27 06:09:27 +0900

    Added a new class.

---
+++ Movox/Main/MVNetworkConnector.m
@@ -0,0 +1,13 @@
+//
+//  MVNetworkConnector.m
+//  Movox
+//
+//  Created by 哲史 深谷 on 12/07/27.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "MVNetworkConnector.h"
+
+@implementation MVNetworkConnector
+
+@end
