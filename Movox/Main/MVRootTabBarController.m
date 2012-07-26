f7eea87ac9a05b55f7d315aeb2510c571dd6ee67
akiii <akiii@sfc.wide.ad.jp>
2012-07-27 05:38:05 +0900

    Added new tab views.

--- Movox/Main/MVRootTabBarController.m
+++ Movox/Main/MVRootTabBarController.m
@@ -7,26 +7,63 @@
 //
 
 #import "MVRootTabBarController.h"
+#import "MVNavigationController.h"
+
+#import "MVRootFriendsViewController.h"
+#import "MVRootCameraViewController.h"
 #import "MVRootMainViewController.h"
 #import "MVRootSecondViewController.h"
 #import "MVRootSettingsViewController.h"
 
+typedef enum MVRootTabBarTag {
+    MVRootTabBarTagFriends  = 0,
+    MVRootTabBarTagMain2    = 1,
+    MVRootTabBarTagCamera   = 2,
+    MVRootTabBarTagSecond   = 3,
+    MVRootTabBarTagSettings = 4
+} _MVRootTabBarTag;
+
+@interface MVRootTabBarController()
+@property (assign) int preViewControllerTag;
+@property (strong, nonatomic) MVRootCameraViewController *cameraViewController;
+@end
+
 @implementation MVRootTabBarController
+@synthesize preViewControllerTag;
+@synthesize cameraViewController;
 
 - (id)init{
     if (self = [super init]) {
-        MVRootMainViewController *mainViewController = [[MVRootMainViewController alloc] initWithNibName:nil bundle:nil];
-        mainViewController.tabBarItem = [[UITabBarItem alloc] initWithTabBarSystemItem:UITabBarSystemItemHistory tag:0];
+        MVNavigationController *friendsNavigationController = [MVNavigationController navigationControllerWithRootViewController:[[MVRootFriendsViewController alloc] initWithNibName:nil bundle:nil] tabBarTitle:@"friends" tabBarImageName:@"friends.png" tabBarTag:MVRootTabBarTagFriends];
+        
+        MVRootMainViewController *mainViewController2 = [[MVRootMainViewController alloc] initWithNibName:nil bundle:nil];
+        mainViewController2.tabBarItem = [[UITabBarItem alloc] initWithTabBarSystemItem:UITabBarSystemItemMostViewed tag:MVRootTabBarTagMain2];
+
+        self.cameraViewController = [[MVRootCameraViewController alloc] initWithNibName:nil bundle:nil];
+        self.cameraViewController.tabBarItem = [[UITabBarItem alloc] initWithTabBarSystemItem:UITabBarSystemItemContacts tag:MVRootTabBarTagCamera];
+        self.cameraViewController.onCloseCamera = ^(){
+            self.selectedIndex = self.preViewControllerTag;
+        };
         
         MVRootSecondViewController *secondViewController = [[MVRootSecondViewController alloc] initWithNibName:nil bundle:nil];
-        secondViewController.tabBarItem = [[UITabBarItem alloc] initWithTabBarSystemItem:UITabBarSystemItemFeatured tag:1];
+        secondViewController.tabBarItem = [[UITabBarItem alloc] initWithTabBarSystemItem:UITabBarSystemItemFeatured tag:MVRootTabBarTagSecond];
         
-        MVRootSettingsViewController *settingsViewController = [[MVRootSettingsViewController alloc] initWithNibName:nil bundle:nil];
-        settingsViewController.tabBarItem = [[UITabBarItem alloc] initWithTitle:@"settings" image:nil tag:2];
+        MVNavigationController *settingsNavigationController = [MVNavigationController navigationControllerWithRootViewController:[[MVRootSettingsViewController alloc] initWithNibName:nil bundle:nil] tabBarTitle:@"settings" tabBarImageName:@"gear.png" tabBarTag:MVRootTabBarTagSettings];
         
-        [self setViewControllers:[NSArray arrayWithObjects:mainViewController, secondViewController, settingsViewController, nil] animated:NO];
+        [self setViewControllers:[NSArray arrayWithObjects:friendsNavigationController, mainViewController2, cameraViewController, secondViewController, settingsNavigationController, nil] animated:NO];
+
+        self.preViewControllerTag = MVRootTabBarTagMain2;
+        self.selectedIndex = MVRootTabBarTagMain2;
     }
     return self;
+}
+
+- (void)tabBar:(UITabBar *)tabBar didSelectItem:(UITabBarItem *)item{
+    if (item.tag == MVRootTabBarTagCamera) {
+        [self.cameraViewController showCameraWithAnimation:YES];
+    }else {
+        self.preViewControllerTag = item.tag;
+    }
 }
 
 @end
